c307b3ead7187443784f16c6ade2ae3f24e61a8e
Rohan Nobis <nobisro@gmail.com>
2018-12-02 17:36:23 -0500

    added route for post request to insert into db

--- server/index.js
+++ server/index.js
@@ -1,13 +1,23 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+// const routes = require("./routes");
+const db = require("../database/index.js");
+const Bubble = require("../database/bubble.js");
 
 const PORT = 1337;
 
 app.use(express.static(path.join(__dirname, "/../client/dist")));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "/../dist/index.html"));
+  res.sendFile(path.join(__dirname, "/../dist"));
+});
+
+app.get("/api/post", (req, res) => {
+  Bubble.create(req.body).then(() => {
+    console.log("bubble created");
+    db.disconnect();
+  });
 });
 
 app.listen(PORT, () => {
