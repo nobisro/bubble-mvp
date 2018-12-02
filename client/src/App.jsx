9e0de94ff510e4df370b8aa8da89f8c80b7e05ad
Rohan Nobis <nobisro@gmail.com>
2018-12-01 19:41:27 -0500

    Basic components to play with posed library

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,19 @@
-import react from "react";
-import reactDOM from "react-dom";
-import posed from "react-pose";
-import Box from "./Box";
+// import react from "react";
+// import reactDOM from "react-dom";
+// import posed from "react-pose";
+// import Box from "./Box";
 
-// const App = () => <Box className="box" />;
-const App = () => <h1>hey</h1>;
+// // const App = () => <Box className="box" />;
+// const App = () => <h1>HEY</h1>;
 
-const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+// const rootElement = document.getElementById("root");
+// ReactDOM.render(<App />, rootElement);
+
+import React from "react";
+import { render } from "react-dom";
+class App extends React.Component {
+  render() {
+    return <div>'Hello world!'</div>;
+  }
+}
+render(<App />, document.getElementById("root"));
