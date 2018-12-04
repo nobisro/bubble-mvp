f443fba4aedc2840695f5bbda5ef8982978589ba
Rohan Nobis <nobisro@gmail.com>
2018-12-03 22:16:43 -0500

    added handleBubbleClick event

--- components/App.jsx
+++ components/App.jsx
@@ -5,6 +5,7 @@ import Bubble from "./Bubble.jsx";
 import Fizz from "./Fizz.jsx";
 import Header from "./Header.jsx";
 import axios from "axios";
+import Input from "./Input.jsx";
 
 export default function App() {
   const [bubble, setBubble] = useState("");
@@ -15,6 +16,7 @@ export default function App() {
       axios
         .get("/api/bubbles")
         .then(response => {
+          console.log(response);
           let temp = [];
           response.data.forEach(bubble => {
             temp.push(bubble.body);
@@ -33,42 +35,60 @@ export default function App() {
   }
 
   function handleSave(bubble) {
-    console.log("fizz len: ", fizz.length);
-    axios.post("/api/bubbles", { body: bubble, color: "blue" }).then(bubble => {
+    axios.post("/api/bubbles", { body: bubble, id: "blue" }).then(bubble => {
       let updatedFizz = fizz;
       updatedFizz.push(bubble.body);
       setFizz(updatedFizz);
-      console.log("~~~~post finished");
     });
-    // .then(response => {
-    //   let updatedFizz = fizz;
-    //   updatedFizz.push(bubble);
-    //   console.log("updatedFizz len: ", updatedFizz.length);
-    //   setFizz(updatedFizz);
-    //   console.log("line 42");
-    // });
   }
 
   function clearAll() {
-    axios.delete("/api/bubbles").then(response => {
-      console.log(response);
-    });
+    axios
+      .delete("/api/bubbles")
+      .then(() => {
+        console.log("bubbles deleted");
+      })
+      .catch(err => {
+        console.error(err);
+      });
     setFizz([]);
   }
 
+  function handleBubbleClick(id) {
+    let updatedFizz = fizz;
+    updatedFizz = updatedFizz.filter((bubble, element) => {
+      id !== element;
+    });
+    console.log(updatedFizz);
+    console.log(fizz);
+  }
+
   let bubbles = fizz.map((bubble, id) => {
-    return <Bubble text={bubble} id={id} />;
+    return (
+      <Bubble text={bubble} id={id} handleBubbleClick={handleBubbleClick}>
+        <input>hello</input>
+      </Bubble>
+    );
   });
 
   return (
     <React.Fragment>
+      {" "}
+      <Header
+        clearAll={clearAll}
+        handleChange={handleChange}
+        handleSave={handleSave}
+      />
+      {bubbles}
+    </React.Fragment>
+  );
+}
+
+/*
       <Header
         clearAll={clearAll}
         handleChange={handleChange}
         handleSave={handleSave}
       />
 
-      {bubbles}
-    </React.Fragment>
-  );
-}
+*/
